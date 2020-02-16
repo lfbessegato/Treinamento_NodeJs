0057524ec6da44e618e2c9b2f7e452eda6fb9549
Luciano Bessegato <lfrbessegato@gmail.com>
2020-02-16 16:57:25 -0300

    Capitulo07 - Integração com o Banco de dados

---
+++ Capitulo7/ntalk/models/usuario.js
@@ -0,0 +1,22 @@
+/* global db */
+const Schema = require('mongoose').Schema;
+
+module.exports = () => {
+  const contato = Schema({
+    nome: String,
+    email: String
+  });
+  const usuario = Schema({
+    nome: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      index: { unique: true }
+    },
+    contatos: [contato]
+  });
+  return db.model('usuarios', usuario);
+};
